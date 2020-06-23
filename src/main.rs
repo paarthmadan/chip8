e9aeb99c3a0024d9faba7e739b09cfb2df7c6edf
Paarth Madan <paarth.madan@shopify.com>
2020-06-23 12:50:16 -0400

    Display struct, basic instruction fetch

--- src/main.rs
+++ src/main.rs
@@ -5,9 +5,31 @@ struct Chip8 {
     memr_addr: usize,
     delay: u8,
     sound: u8,
-    pc: u16,
+    pc: usize,
     sp: usize,
     stack: [u16; 16],
+    display: Display,
+}
+
+struct Display {
+    lcd: [[char; 64]; 32]
+}
+
+impl Display {
+    fn dump(&self) {
+        for row in &self.lcd {
+            for c in row.iter() {
+                print!("{}", c);
+            }
+            println!("");
+        }
+    }
+}
+
+impl Default for Display {
+    fn default() -> Self {
+        Display { lcd: [[' '; 64]; 32] }
+    }
 }
 
 struct Cartridge {
@@ -18,8 +40,27 @@ impl Chip8 {
         // load program into memory
     }
 
+    fn load_word(&self, ptr: usize) -> u8 {
+        self.memory[ptr]
+    }
+
     fn run(&mut self) {
         // Instruction Fetch (Load instruction from memory)
+        let upper_word = self.load_word(self.pc);
+        let lower_word = self.load_word(self.pc + 1);
+
+        let a = upper_word >> 4;
+        let b = (0x0F) & upper_word;
+        let c = lower_word >> 4;
+        let d = (0x0F) & lower_word;
+
+        match (a, b, c, d) {
+            (0, 0, 0xE, 0) => println!("clear"),
+            (0, 0, 0xE, 0xE) => println!("ret"),
+            (1, _, _, _) => println!("jump"),
+            (2, _, _, _) => println!("call"),
+            _ => unreachable!("Instruction not supported: {:x?}{:x?}{:x?}{:x?}", a, b, c, d),
+        }
         // Instruction Decode (Figure out what to do)
         // Instruction Execute
     }
@@ -37,12 +78,16 @@ impl Default for Chip8 {
             pc: 0,
             sp: 0,
             stack: [0; 16],
+            display: Display::default(),
         }
     }
 }
 
 
 fn main () {
-    let chip = Chip8::default();
+    let mut chip = Chip8::default();
+    chip.memory[0] = 0x00;
+    chip.memory[1] = 0x00;
     println!("Chip8 Emulator!");
+    chip.run();
 }
